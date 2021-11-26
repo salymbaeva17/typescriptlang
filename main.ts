56213a9f4b04196a15f9227a45267a5623984035
salymbaeva17 <salymbaeva17@gmail.com>
2021-11-27 00:17:03 +0600

    ArraysAndObjects

--- main.ts
+++ main.ts
@@ -28,3 +28,15 @@ let isArray: any[] = [45,65,65, "45", "87", true]
 let isArray1: Array<number> = [45,65,65, "45", "87", true]
 // Type 'string' is not assignable to type 'number'.
 // Type 'boolean' is not assignable to type 'number'.
+
+// несколько типов данных в массиве
+// в первом случае строго важна последовательность
+// а в другом просто определяем типы которые можно добавлять в массив
+
+let isArray2: [number, number,number, string,string, boolean] = [45,65,65, "45", "87", true]
+let isArray3: Array<number | string> = [45,65,65, "45", "87"]
+
+
+// let isObject : object = 4 // Type 'number' is not assignable to type 'object'.
+let isObject : object = {} // хреновый вариант
+
